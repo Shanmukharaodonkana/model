dc1289e23035a8df028ca3f859f3b2ba94ace6a9
Shanmukhraudomkana <donkanashanmukhrao2000@gmail.com>
2022-04-09 13:53:55 +0530

    fjfjfkk

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('car_price_predictor.pkl', 'rb'))
+model = pickle.load(open('C:\\Users\\donka\\OneDrive\\Desktop\\model-main\\newmodel.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('home.html')
